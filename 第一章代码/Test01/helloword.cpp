1e6d87d76c9b6db2345f9439b023df59dbd3b6f6
Yanxugit <86583315+Yanxugit@users.noreply.github.com>
2021-06-28 11:43:16 +0800

    Add files via upload

---
+++ 第一章代码/Test01/helloword.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance ,HINSTANCE hprevInstance,PSTR szCmdLine,int iCmdShow) 
+{
+	
+	MessageBox(NULL,TEXT("这是阎旭的第一程序啊啊啊！"),TEXT("标题"),0);
+	return 0;
+}
